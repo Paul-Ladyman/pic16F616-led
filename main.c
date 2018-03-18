ac4d529442b8ce1dddcb306e3e63f4b756110538
Paul-Ladyman <ladyman.paul@gmail.com>
2018-03-18 18:02:07 +0000

    Use Timer0 interrupt to blink LED

--- main.c
+++ main.c
@@ -9,44 +9,56 @@
 
 #include "configuration.h"
 
-// Define three pins from PortA to use to read buttons
-#define BUTTON0 RA0
-#define BUTTON1 RA1
-#define BUTTON2 RA2
 // Define three pins from PortC to use as outputs for LEDs
 #define LED0 RC3
-#define LED1 RC4
-#define LED2 RC5
+
+int count = 0;
 
 void initMain() {
     // Set ports as digital I/O, not analogue input
     ANSEL = 0b00000000;
 
-    /* 
-     * TRISA is the data direction register for PORTA.
-     * Here we set RA0, RA1 and RA2 to be a digital input
-     * and all other pins to be outputs (0)
-     */
-    TRISA = 0b00000111;
-    
-    // Enable weak pull-ups globally
-    OPTION_REGbits.nRAPU = 0;
-    
-    // Enable weak pull-ups for for RA0, RA1, RA2
-    WPUA = 0b00000111;
     /*
      * TRISC is the data direction register for PORTC.
      * Here we set all pins to be outputs (0)
      */
     TRISC = 0;
+
+    // Set to timer mode (internal clock source)
+    OPTION_REGbits.T0CS = 0;
+
+    // Assign Prescaler to TIMER0 instead of WatchDogTimer 
+    OPTION_REGbits.PSA = 0;
+    // Set Prescaler to 256
+    OPTION_REGbits.PS = 0b111;
+
+    // enable Timer0 interrupt
+    INTCONbits.T0IE = 1;
+    // Enable global interrupts
+    INTCONbits.GIE = 1;
 }
 
 void main(void) {
     initMain();
-    
-    while(1) {
-        LED0 = BUTTON0;
-        LED1 = BUTTON1;
-        LED2 = BUTTON2;
+
+    while (1);
+}
+
+void interrupt ISR(void) {
+    static int count = 0;
+
+    // Reset timer flag
+    INTCONbits.T0IF = 0;
+
+    count++;
+
+    // 15 timer counts = 1Hz, 3 counts = 5Hz
+    // Given 4MHz osc, 1:256 prescaler, 8-bit Timer (256) with no offset
+    // count = 4MHz / 4 / 256 / 256 / 5
+    if (count == 3) {
+        LED0 = 1;
+        count = 0;
+    } else {
+        LED0 = 0;
     }
 }
