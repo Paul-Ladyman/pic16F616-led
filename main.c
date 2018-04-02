f24101b33585702b2199178a55aebdfc67631588
Paul-Ladyman <ladyman.paul@gmail.com>
2018-04-02 15:39:49 +0100

    Use CCP module to provide PWM output

--- main.c
+++ main.c
@@ -9,44 +9,44 @@
 
 #include "configuration.h"
 
-// Define three pins from PortA to use to read buttons
-#define BUTTON0 RA0
-#define BUTTON1 RA1
-#define BUTTON2 RA2
-// Define three pins from PortC to use as outputs for LEDs
-#define LED0 RC3
-#define LED1 RC4
-#define LED2 RC5
-
 void initMain() {
     // Set ports as digital I/O, not analogue input
     ANSEL = 0b00000000;
-
-    /* 
-     * TRISA is the data direction register for PORTA.
-     * Here we set RA0, RA1 and RA2 to be a digital input
-     * and all other pins to be outputs (0)
-     */
-    TRISA = 0b00000111;
     
-    // Enable weak pull-ups globally
-    OPTION_REGbits.nRAPU = 0;
-    
-    // Enable weak pull-ups for for RA0, RA1, RA2
-    WPUA = 0b00000111;
     /*
      * TRISC is the data direction register for PORTC.
      * Here we set all pins to be outputs (0)
      */
     TRISC = 0;
+    
+    // Turn on Timer2 - this Timer is used by the CCP module's PWM mode
+    T2CONbits.TMR2ON = 1;
+    
+    // Set Timer2 prescaler value to 1:1
+    T2CONbits.T2CKPS = 0b00;
+    
+    // Set Timer 2 period value
+    PR2 = 255;
+    
+    // Select CCP PWM mode
+    CCP1CONbits.CCP1M = 0b1100;
+    
+    // Set 2 LSBs of PWM duty cycle
+    CCP1CONbits.DCB = 0b00;
 }
 
 void main(void) {
     initMain();
     
+    int i = 0;
+    
     while(1) {
-        LED0 = BUTTON0;
-        LED1 = BUTTON1;
-        LED2 = BUTTON2;
+        // Set 8 MSBs of PWM duty cycle
+        CCPR1L = i;
+        i++;
+        if (i > 255) {
+            i = 0;
+        }
+        __delay_ms(10);
     }
 }
